73398f792365f55a3e3b594795c40b26f1c034ed
doguhanniltextra <doguhangithub@gmail.com>
2025-07-13 04:56:26 +0300

    feat: RestaurantRepository created

---
+++ src/main/java/com/doguy/restaurant/repositories/RestaurantRepository.java
@@ -0,0 +1,10 @@
+package com.doguy.restaurant.repositories;
+
+import com.doguy.restaurant.domain.entities.Restaurant;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepository extends ElasticsearchRepository<Restaurant, String> {
+    
+}
