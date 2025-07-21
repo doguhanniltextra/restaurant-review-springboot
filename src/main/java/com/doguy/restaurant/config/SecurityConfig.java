02718a6026ed5bf90678f1d35bbf8a370474ceb3
doguhanniltextra <doguhangithub@gmail.com>
2025-07-21 06:46:20 +0300

    feat(config): photo endpoint added

--- src/main/java/com/doguy/restaurant/config/SecurityConfig.java
+++ src/main/java/com/doguy/restaurant/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.doguy.restaurant.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -15,6 +16,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.GET, "/api/photos/**")
+                        .permitAll()
                 .anyRequest()
                 .authenticated())
                 .oauth2ResourceServer(oauth2 ->
