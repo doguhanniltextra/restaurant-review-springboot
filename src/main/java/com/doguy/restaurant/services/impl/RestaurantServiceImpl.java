0ef3740e9f1266572139d582f390ddb45213aea7
doguhanniltextra <doguhangithub@gmail.com>
2025-07-22 07:35:06 +0300

    feat(services/impl): searching restaurant added

--- src/main/java/com/doguy/restaurant/services/impl/RestaurantServiceImpl.java
+++ src/main/java/com/doguy/restaurant/services/impl/RestaurantServiceImpl.java
@@ -6,6 +6,8 @@ import com.doguy.restaurant.repositories.RestaurantRepository;
 import com.doguy.restaurant.services.GeoLocationService;
 import com.doguy.restaurant.services.RestaurantService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.core.geo.GeoPoint;
 import org.springframework.stereotype.Service;
 
@@ -45,5 +47,26 @@ public class RestaurantServiceImpl implements RestaurantService {
 
         Restaurant save = restaurantRepository.save(build);
         return save;
+    }
+
+    @Override
+    public Page<Restaurant> searchRestaurant(
+            String query, Float minRating, Float latitude,
+            Float longitude, Float radius, Pageable pageable) {
+
+        if(null != minRating && (null == query || query.isEmpty())) {
+            return restaurantRepository.findByAverageRatingGreaterThanEqual(minRating, pageable);
+        }
+
+        Float searchMinRating = null == minRating ? 0f : minRating;
+        if(null != query && !query.trim().isEmpty()) {
+            return restaurantRepository.findByQueryAndMinRating(query,searchMinRating, pageable);
+        }
+
+        if(null != latitude && null != longitude && null !=radius) {
+            return restaurantRepository.findNearbyRestaurants(latitude,longitude,radius, pageable);
+        }
+
+        return restaurantRepository.findAll(pageable);
     }
 }
