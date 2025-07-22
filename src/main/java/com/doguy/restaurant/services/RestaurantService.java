1e269a5edb110aa510c61517804e3c1aa6aa1406
doguhanniltextra <doguhangithub@gmail.com>
2025-07-22 07:57:34 +0300

    feat: updateRestaurant method added

--- src/main/java/com/doguy/restaurant/services/RestaurantService.java
+++ src/main/java/com/doguy/restaurant/services/RestaurantService.java
@@ -2,7 +2,25 @@ package com.doguy.restaurant.services;
 
 import com.doguy.restaurant.domain.entities.Restaurant;
 import com.doguy.restaurant.domain.entities.RestaurantCreateUpdateRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
 
 public interface RestaurantService {
     Restaurant createRestaurant(RestaurantCreateUpdateRequest request);
+
+    Page<Restaurant> searchRestaurant(
+            String query,
+            Float minRating,
+            Float latitude,
+            Float longitude,
+            Float radius,
+            Pageable pageable
+    );
+
+
+    Optional<Restaurant> getRestaurant(String id);
+
+    Restaurant updateRestaurant(String id, RestaurantCreateUpdateRequest restaurantCreateUpdateRequest);
 }
