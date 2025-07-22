11034d3489c5c9c239c6233ff45660510d6cb0d5
doguhanniltextra <doguhangithub@gmail.com>
2025-07-22 07:49:56 +0300

    feat: making public api/restaurant

--- src/main/java/com/doguy/restaurant/config/SecurityConfig.java
+++ src/main/java/com/doguy/restaurant/config/SecurityConfig.java
@@ -16,6 +16,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.GET, "/api/restaurants/**")
+                        .permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/photos/**")
                         .permitAll()
                 .anyRequest()
